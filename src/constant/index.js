40e4e721dee7d01f61af99c18fcb77b357e45b1d
HEIWMM <1026957234@qq.com>
2021-05-07 11:34:53 +0800

    添加常量文件

---
+++ src/constant/index.js
@@ -0,0 +1,19 @@
+export const EmergencyDegree = {
+  时间宽裕: 4,
+  时间足够: 5,
+  时间有限: 6,
+  时间紧张: 7,
+  马上完成: 8,
+}
+export const ImportantDegree = {
+  意义很小: 4,
+  意义较小: 5,
+  意义一般: 6,
+  意义重要: 7,
+  极其重要: 8,
+}
+export const RiskDegree = {
+  高风险: 4,
+  中风险: 6,
+  低风险: 8,
+}
